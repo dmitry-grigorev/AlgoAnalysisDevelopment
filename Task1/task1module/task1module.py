947c0fe0e769ced484a4a5bed8027e087334a59a
dmitry-grigorev <crafting20002033@gmail.com>
2022-09-11 23:10:46 +0300

    Task 1 final commit (I hope)

--- Task1/task1module/task1module.py
+++ Task1/task1module/task1module.py
@@ -52,13 +52,13 @@ def calc_polynom_horner(coefs: np.ndarray, x):
     return s
 
 
-def bubble_sort(v: np.ndarray):
+def bubble_sort(v):
     has_swapped = True
     num_of_iterations = 0
 
     while has_swapped:
         has_swapped = False
-        for i in range(v.shape[0] - num_of_iterations - 1):
+        for i in range(len(v) - num_of_iterations - 1):
             if v[i] > v[i + 1]:
                 v[i], v[i + 1] = v[i + 1], v[i]
                 has_swapped = True
@@ -66,27 +66,24 @@ def bubble_sort(v: np.ndarray):
     return v
 
 
-def quick_sort(v: np.ndarray):
-    quick_sort_r(v, 0, v.shape[0] - 1)
+def quick_sort(v):
+    quick_sort_r(v, 0, len(v) - 1)
 
 
 def quick_sort_r(v, start, end):
     if len(v) == 1:
         return
-    # Create an auxiliary stack
+
     size = end - start + 1
     stack = [0] * size
 
-    # initialize top of stack
     top = -1
 
-    # push initial values of l and h to stack
     top = top + 1
     stack[top] = start
     top = top + 1
     stack[top] = end
 
-    # Keep popping from stack while is not empty
     while top >= 0:
 
         # Pop h and l
@@ -95,20 +92,14 @@ def quick_sort_r(v, start, end):
         start = stack[top]
         top = top - 1
 
-        # Set pivot element at its correct position in
-        # sorted array
         p = partition(v, start, end)
 
-        # If there are elements on left side of pivot,
-        # then push left side to stack
         if p - 1 > start:
             top = top + 1
             stack[top] = start
             top = top + 1
             stack[top] = p - 1
 
-        # If there are elements on right side of pivot,
-        # then push right side to stack
         if p + 1 < end:
             top = top + 1
             stack[top] = p + 1
@@ -122,7 +113,6 @@ def partition(v, start, end):
 
     for j in range(start, end):
         if v[j] <= x:
-            # increment index of smaller element
             i = i + 1
             v[i], v[j] = v[j], v[i]
 
@@ -131,14 +121,6 @@ def partition(v, start, end):
 
 
 def calc_min_run(length, minMerge):
-    """Returns the minimum length of a
-        run from 23 to 64 so that
-        the len(array)/minrun is less than or
-        equal to a power of 2.
-
-        e.g. 1=>1, ..., 63=>63, 64=>32, 65=>33,
-        ..., 127=>64, 128=>32, ...
-        """
     r = 0
     while length >= minMerge:
         r |= length & 1
@@ -155,11 +137,9 @@ def insertion_sort(v, left, right):
 
 
 def merge(v, left, mid, right):
-    # original array is broken in two parts
-    # left and right array
     len1 = mid - left + 1
     len2 = right - mid
-    leftArr, rightArr = np.zeros(shape = len1) , np.zeros(shape = len2)
+    leftArr, rightArr = [0] * len1, [0] * len2
     for i in range(0, len1):
         leftArr[i] = v[left + i]
     for i in range(0, len2):
@@ -167,8 +147,6 @@ def merge(v, left, mid, right):
 
     i, j, k = 0, 0, left
 
-    # after comparing, we merge those two array
-    # in larger sub array
     while i < len1 and j < len2:
         if leftArr[i] <= rightArr[j]:
             v[k] = leftArr[i]
@@ -180,13 +158,11 @@ def merge(v, left, mid, right):
 
         k += 1
 
-    # Copy remaining elements of left, if any
     while i < len1:
         v[k] = leftArr[i]
         k += 1
         i += 1
 
-    # Copy remaining element of right, if any
     while j < len2:
         v[k] = rightArr[j]
         k += 1
@@ -194,7 +170,7 @@ def merge(v, left, mid, right):
 
 
 def tim_sort(v: np.ndarray):
-    length = v.shape[0]
+    length = len(v)
     minRun = calc_min_run(length, 32)
     for start in range(0, length, minRun):
         end = min(start + minRun - 1, length - 1)
@@ -203,40 +179,34 @@ def tim_sort(v: np.ndarray):
     size = minRun
     while size < length:
 
-        # Pick starting point of left sub array. We
-        # are going to merge arr[left..left+size-1]
-        # and arr[left+size, left+2*size-1]
-        # After every merge, we increase left by 2*size
         for left in range(0, length, 2 * size):
 
-            # Find ending point of left sub array
-            # mid+1 is starting point of right sub array
             mid = min(length - 1, left + size - 1)
             right = min((left + 2 * size - 1), (length - 1))
 
-            # Merge sub array arr[left.....mid] &
-            # arr[mid+1....right]
             if mid < right:
                 merge(v, left, mid, right)
 
         size = 2 * size
 
 
-def matrix_multiplication(A: np.ndarray, B: np.ndarray):
-    n = A.shape[0]
-    C = np.zeros(shape=(n, n))
+def matrix_multiplication(A, B):
+    n = len(A)
+    C = [[0]*n]*n
     for i in range(n):
         for j in range(n):
             for k in range(n):
-                C[i, j] += A[i, k] * B[k, j]
+                C[i][j] += A[i][k] * B[k][j]
     return C
 
 
 def test_time_complexity(function, args, n):
     exectime = np.zeros(n, dtype=float)
+    v, fargs = args[0].tolist(), args[1:]
     for i in range(1, n + 1):
+        vect = v[:i]
         start = timeit.default_timer()
-        function((args[0])[:i], *(args[1:]))
+        function(vect, *fargs)
         exectime[i - 1] = (timeit.default_timer() - start) * 1000
     return exectime
 
@@ -246,7 +216,7 @@ def plot_exectime(times: np.ndarray, fitfunc):
     n_seq = np.linspace(1, n, n)
     fit = np.polyfit(fitfunc(n_seq), times, 1)
     print(fit[0])
-    return ggplot() + geom_point(aes(x=n_seq, y=times)) + xlab("Array size") + \
+    return fit[0], ggplot() + geom_point(aes(x=n_seq, y=times)) + xlab("Array size") + \
            ylab("Execution time (in milliseconds)") + geom_line(aes(x=n_seq, y=fit[1] + fit[0] * fitfunc(n_seq)),
                                                                 color="red", size=2, alpha=0.5)
 
@@ -255,16 +225,17 @@ def plot_algo_complexity_info(func, fitfunc, args=list()):
     times = np.zeros(2000, dtype=float)
     for _ in range(5):
         times += test_time_complexity(func, [np.random.uniform(size=2000)] + args, 2000)
-    times /= 5
-    return plot_exectime(times, fitfunc)
+    return plot_exectime(times / 5, fitfunc)
 
 
-def test_mul_complexity(A: np.ndarray, B: np.ndarray):
-    n = A.shape[0]
+def test_mul_complexity(A, B):
+
+    n = len(A)
+    listA, listB = A.tolist(), B.tolist()
     exectime = np.zeros(n, dtype=float)
     for i in range(1, n + 1):
         start = timeit.default_timer()
-        matrix_multiplication(A[:i, :i], B[:i, :i])
+        matrix_multiplication(listA[:i][:i], listB[:i][:i])
         exectime[i - 1] = (timeit.default_timer() - start) * 1000
     return exectime
 
@@ -273,5 +244,4 @@ def plot_mul_complexity_info():
     times = np.zeros(100, dtype=float)
     for _ in range(5):
         times += test_mul_complexity(np.random.uniform(size=(100, 100)), np.random.uniform(size=(100, 100)))
-    times /= 5
-    return plot_exectime(times, lambda x: x ** 3)
+    return plot_exectime(times / 5, lambda x: x ** 3)
