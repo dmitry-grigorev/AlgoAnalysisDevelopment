eebe93d30e563b2f6b3c2a12542aa1900f557b06
dmitry-grigorev <crafting20002033@gmail.com>
2022-09-20 21:36:23 +0300

    Contour plots for target function are provided

--- Task3/task3module/task3module.py
+++ Task3/task3module/task3module.py
@@ -3,12 +3,22 @@ from Task2.task2module.task2module import goldenratio_method
 from _collections_abc import Sequence
 
 
-def squares(x: np.ndarray, y: np.ndarray):
+def squares(x, y):
     return np.sum(np.square(x - y))
 
 
 def belongs(x: np.ndarray, area: np.ndarray):
     return np.all(x > area[:, 0]) and np.all(x < area[:, 1])
+
+
+def projectiononborder(x: np.ndarray, area):
+    areacenter = (area[:, 0] + area[:, 1]) / 2
+    y = np.subtract(x, areacenter)
+    absx = np.abs(y)
+    imaxdiff = np.where(absx == np.max(absx))
+    maxdiff = absx[imaxdiff]
+    size = (area[:, 1] - area[:, 0])[imaxdiff]
+    return areacenter + y * size / maxdiff / 2
 
 
 """
@@ -20,13 +30,13 @@ If optimal point 'optpoint' is provided, this function provides l2-norm precisio
 
 
 def newtons(func, limits, x0, funcgrad, funchess, eps=1e-3, optpoint=None):
-    next, prev = x0, None
+    curr, prev = x0, None
     niters, fcalcs, gradcalcs, hesscalcs, matrixinvers = 0, 0, 0, 0, 0
-    while (prev is None or np.linalg.norm(next - prev) > eps) and belongs(next, limits):
-        g, H = funcgrad(next), funchess(next)
+    while (prev is None or np.linalg.norm(curr - prev) > eps) and belongs(curr, limits):
+        g, H = funcgrad(curr), funchess(curr)
         delta = -np.linalg.inv(H) @ g
-        prev = next
-        next = next + delta
+        prev = curr
+        curr = curr + delta
         niters += 1
         matrixinvers += 1
         gradcalcs += 1
@@ -38,10 +48,13 @@ def newtons(func, limits, x0, funcgrad, funchess, eps=1e-3, optpoint=None):
                   "hesscalcs": hesscalcs,
                   "matrixinversions": matrixinvers}
 
+    if not belongs(curr, limits):
+        curr = projectiononborder(curr, limits)
+
     if optpoint is None:
-        return next, func(next), calcsstats, None
+        return curr, func(curr), calcsstats, None
     else:
-        return next, func(next), calcsstats, np.linalg.norm(next - optpoint)
+        return curr, func(curr), calcsstats, np.linalg.norm(curr - optpoint)
 
 
 """
@@ -56,14 +69,14 @@ If optimal point 'optpoint' is provided, this function provides l2-norm precisio
 
 def levenmarq(func, limits, x0, regulpar: Sequence[int, float, tuple[float, float]],
               funcgrad, funchess, eps=1e-3, optpoint=None):
-    next, prev = x0, None
+    curr, prev = x0, None
     delta = None
     niters, fcalcs, gradcalcs, hesscalcs, matrixinvers = 0, 0, 0, 0, 0
     n = x0.shape[0]
     identitymatrix = np.eye(N=n, dtype=float)
 
-    while (prev is None or np.linalg.norm(next - prev) > eps) and belongs(next, limits):
-        g, H = funcgrad(next), funchess(next)
+    while (prev is None or np.linalg.norm(curr - prev) > eps) and belongs(curr, limits):
+        g, H = funcgrad(curr), funchess(curr)
         gradcalcs += 1
         hesscalcs += 1
         if isinstance(regulpar, float) or isinstance(regulpar, int):
@@ -76,9 +89,12 @@ def levenmarq(func, limits, x0, regulpar: Sequence[int, float, tuple[float, floa
             delta = -np.linalg.inv(H + linesearchres[0] * identitymatrix) @ g
             matrixinvers += (1 + linesearchres[2])
 
-        prev = next
-        next = next + delta
+        prev = curr
+        curr = curr + delta
         niters += 1
+
+    if not belongs(curr, limits):
+        curr = projectiononborder(curr, limits)
 
     calcsstats = {"iterations": niters,
                   "funccalcs": fcalcs,
@@ -87,6 +103,6 @@ def levenmarq(func, limits, x0, regulpar: Sequence[int, float, tuple[float, floa
                   "matrixinversions": matrixinvers}
 
     if optpoint is None:
-        return next, func(next), calcsstats, None
+        return curr, func(curr), calcsstats, None
     else:
-        return next, func(next), calcsstats, np.linalg.norm(next - optpoint)
+        return curr, func(curr), calcsstats, np.linalg.norm(curr - optpoint)
