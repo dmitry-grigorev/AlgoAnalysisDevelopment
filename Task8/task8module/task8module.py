5beb05c639b969f4e4a931c8949506e0d7123654
dmitry-grigorev <crafting20002033@gmail.com>
2022-10-19 23:13:32 +0300

    Insignificant changes in task 8

--- Task8/task8module/task8module.py
+++ Task8/task8module/task8module.py
@@ -1,6 +1,8 @@
 import numpy as np
 from typing import List
 import copy
+import timeit
+import matplotlib.pyplot as plt
 
 
 def generate_from_n_full_weighted_directed_graph(n, possibleweights=None):
@@ -60,3 +62,43 @@ def improved_all_pairs_shortest_paths(Madj: List[list]) -> List[list]:
         L = specialsquare(L)
         m *= 2
     return L
+
+
+def test_time_complexity_APSP(Madj: np.ndarray, n):
+    exectime = np.zeros(n, dtype=float)
+    for i in range(1, n + 1):
+        subMadj = Madj[:i, :i].tolist()
+        start = timeit.default_timer()
+        improved_all_pairs_shortest_paths(subMadj)
+        exectime[i - 1] = (timeit.default_timer() - start) * 1000
+    return exectime
+
+
+def plot_exectime(times: np.ndarray, fitfunc, strfitfunc: str):
+    n = times.shape[0]
+    n_seq = np.linspace(1, n, n)
+    fit = np.polyfit(fitfunc(n_seq), times, 1)
+    print(fit[0])
+
+    fig = plt.figure(facecolor="white")
+    ax = fig.add_subplot(1, 1, 1)
+    plt.scatter(x=n_seq, y=times, c="b")
+    plt.plot(n_seq, fit[1] + fit[0]*fitfunc(n_seq), label = "$y = {}$".format(round(fit[0], 5)*1e5) +\
+                                                            "$\dot 10^{-5}$" + strfitfunc, c="r")
+    plt.xlabel("Number of nodes")
+    plt.ylabel("Execution time (in milliseconds)")
+    plt.legend(bbox_to_anchor=(1.05, 1), loc='upper left')
+    # plt.xticks(rotation = 45)
+    ax.grid(True)
+
+    return None
+    # fit[0], ggplot() + geom_point(aes(x=n_seq, y=times)) + xlab("Array size") + \
+    #  ylab("Execution time (in milliseconds)") + geom_line(aes(x=n_seq, y=fit[1] + fit[0] * fitfunc(n_seq)),
+    #                                                      color="red", size=2, alpha=0.5)
+
+
+def plot_algo_complexity_info(maxn, fitfunc, strfitfunc: str):
+    times = np.zeros(maxn, dtype=float)
+    for _ in range(5):
+        times += test_time_complexity_APSP(generate_from_n_full_weighted_directed_graph(maxn), maxn)
+    return plot_exectime(times / 5, fitfunc, strfitfunc)
